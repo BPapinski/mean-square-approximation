c1637c4d78379009509e8140a735782f6ead4252
Bartek <bartosz.papinski00@gmail.com>
2025-05-05 19:53:39 +0200

    final version

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def rysuj_wielomian_z_punktami(a, x, y, xmin=None, xmax=None, num_points=500):
+def wykres(a, x, y, xmin=None, xmax=None, num_points=500): # rysuje wykres wielomianu, a - wpolczynniki, x - wartosci x, y - wartosci y
     x = np.array(x)
     y = np.array(y)
-
 
     x_plot = np.linspace(xmin, xmax, num_points)
     y_plot = np.zeros_like(x_plot)
 
-    for i, coeff in enumerate(a):
-        y_plot += coeff * x_plot**i
+    power = 0
+    for c in a:
+        y_plot += c * x_plot**power
+        power+=1
 
     plt.figure(figsize=(8, 5))
     plt.plot(x_plot, y_plot, label='Wielomian aproksymujący', color='blue')
@@ -24,30 +25,33 @@ def rysuj_wielomian_z_punktami(a, x, y, xmin=None, xmax=None, num_points=500):
     plt.legend()
     plt.show()
 
-def cholesky_decomposition(A):
+def Cholesky(A): # zwraca dolna macierz trojkatna, rozkladu macierzy symetrycznej A
     n = A.shape[0]
     L = np.zeros_like(A, dtype=float)
 
-    for i in range(n):
-        for j in range(i + 1):
-            sum_ = sum(L[i][k] * L[j][k] for k in range(j))
-            if i == j:
-                diag_value = A[i][i] - sum_
-                if diag_value <= 0:
-                    raise ValueError(f"Macierz nie jest dodatnio określona przy i={i}, wartość: {diag_value}")
-                L[i][j] = np.sqrt(diag_value)
-            else:
-                L[i][j] = (A[i][j] - sum_) / L[j][j]
+    L[0][0] = np.sqrt(A[0][0])
+    for i in range(1, n):
+        L[i][0] = A[i][0] / L[0][0]
+    # mamy ustawiona pierwsza kolumne
+
+    # Uzupełniamy resztę macierzy
+    for i in range(1, n):
+        sum_sq = sum(L[i][k] ** 2 for k in range(i))
+        L[i][i] = np.sqrt(A[i][i] - sum_sq)
+        for j in range(i + 1, n):
+            sum_prod = sum(L[j][k] * L[i][k] for k in range(i))
+            L[j][i] = (A[j][i] - sum_prod) / L[i][i]
     return L
 
-def forward_substitution(L, b):
+
+def forward_substitution(L, b): # rozwiazuje układ dolnotrójkątny
     n = len(b)
     y = np.zeros_like(b)
     for i in range(n):
         y[i] = (b[i] - np.dot(L[i, :i], y[:i])) / L[i][i]
     return y
 
-def backward_substitution(U, y):
+def backward_substitution(U, y): # rozwiazuje uklad gornotrokatny
     n = len(y)
     x = np.zeros_like(y)
     for i in range(n - 1, -1, -1):
@@ -56,13 +60,13 @@ def backward_substitution(U, y):
 
 def rozwiaz_uklad_cholesky(Ata, Y):
     # Rozkład Cholesky'ego: Ata = L * L^T
-    L = cholesky_decomposition(Ata)
+    L = Cholesky(Ata)
 
     # Rozwiąż L * z = Y (układ dolnotrójkątny)
     z = forward_substitution(L, Y)
 
     # Rozwiąż L^T * a = z (układ górnotrójkątny)
-    LT = L.T
+    LT = L.transpose() # transpozycja L
     a = backward_substitution(LT, z)
 
     return a
@@ -82,47 +86,40 @@ def Papinski_Bartosc_MNK(X, Y, n):
     print("transponowana macierz A:")
     print(At)
 
-    print("At * A")
-
     Ata = At.dot(A)
+    print("At * A")
     print(Ata)
-    # Ata - jest dobrze
-
 
     # prawa strona rownania
-    Yarray = []
 
-    #zamiast tej petli mozna zrobic transpozycje jakos, ale chyba sie nie da bo tablica wejsciowa to Y = [1, 2 ,3] a nie [[1, 2, 3]]
-    Yarray = np.array(Y)
-    Prawa = At.dot(Yarray)
+    Prawa = At.dot(np.array(Y))
 
     print("prawa strona rownania")
     print(Prawa)
 
     a = rozwiaz_uklad_cholesky(Ata, Prawa)
-    print("Rozwiązanie a:", a)
     return a
 
 
 
-# wielomian stopnia 2
+# wielomian stopnia 2 f(x) = 2x^2 - 3x + 1
 
 x = [-2, -1, 0, 1, 2]
-y = [15, 6, 1, 0, 1]  # obliczone z f(x) = 2x^2 - 3x + 1
+y = [15, 6, 1, 0, 1]
 
 a = Papinski_Bartosc_MNK(x, y, 2)
 print("Współczynniki wielomianu:", a)
-rysuj_wielomian_z_punktami(a, x, y, xmin=-3, xmax=3)
+wykres(a, x, y, xmin=-3, xmax=3)
 
 
-# wielomian stopnia 3
+# wielomian stopnia 3 f(x) = 2x^3 - x^2 + 3x + 1
 
 x = [-2, -1, 0, 1, 2]
-y = [-25, -5, 1, 5, 19]  # obliczone z f(x) = 2x^3 - x^2 + 3x + 1
+y = [-25, -5, 1, 5, 19]
 
 a = Papinski_Bartosc_MNK(x, y, 3)
 print("Współczynniki wielomianu:", a)
-rysuj_wielomian_z_punktami(a, x, y, xmin=-3, xmax=3)
+wykres(a, x, y, xmin=-3, xmax=3)
 
 # wielomian stopnia 4
 
@@ -131,7 +128,7 @@ y = [-25, -5, 1, 5, 19]
 
 a = Papinski_Bartosc_MNK(x, y, 4)
 print("Współczynniki wielomianu:", a)
-rysuj_wielomian_z_punktami(a, x, y, xmin=-3, xmax=4)
+wykres(a, x, y, xmin=-3, xmax=4)
 
 
 # wielomian 5 stopnia
@@ -141,9 +138,5 @@ y = [-448, -81, -8, -1, 0, 7]
 
 a = Papinski_Bartosc_MNK(x, y, 5)
 print("Współczynniki wielomianu:", a)
-rysuj_wielomian_z_punktami(a, x, y, xmin=-4, xmax=3)
+wykres(a, x, y, xmin=-4, xmax=3)
 
-# testy rozkladu
-#print(cholesky_decomposition(np.array([[4, 0, 0, 0], [0, 9, 0, 0], [0, 0, 4, 0], [0, 0, 0, 16]])))
-#print(cholesky_decomposition(np.array([[4, -6, -8], [-6, 10, -10], [8, -10, 29]])))
-# dziala dobrze
